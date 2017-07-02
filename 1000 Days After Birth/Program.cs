a4d9f04d6edbfb120a063e1d691b7852871d2252
Bozhidar <demona008@abv.bg>
2017-07-02 12:14:07 +0300

    1000 Days After Birth

---
+++ 1000 Days After Birth/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace _1000_Days_After_Birth
+{
+    class Program
+    {
+        static void Main()
+        {
+                        
+            DateTime birthdayDate = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            birthdayDate = birthdayDate.AddDays(999);
+            Console.WriteLine(birthdayDate.ToString("dd-MM-yyyy"));
+
+        }
+    }
+}
