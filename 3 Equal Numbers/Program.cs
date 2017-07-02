9ca98bc6eb6c13afe099165148f57c6bf245a234
Bozhidar <demona008@abv.bg>
2017-07-02 12:08:21 +0300

    3 Equal Numbers
    C#

---
+++ 3 Equal Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace _3_Equal_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var number1 = double.Parse(Console.ReadLine());
+            var number2 = double.Parse(Console.ReadLine());
+            var number3 = double.Parse(Console.ReadLine());
+
+            if ((number1 == number2) && (number2 == number3) && (number1 == number3))
+            {                      
+                Console.WriteLine("yes");                                   
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
