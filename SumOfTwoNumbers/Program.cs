f18f22aa84cd2fed61193e0828889bc0a185918d
Bozhidar <demona008@abv.bg>
2017-07-02 15:12:21 +0300

    Add files via upload

---
+++ SumOfTwoNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+            var counter = 0;
+            for (int i = a; i >= b; i--)
+            {
+                for (int j = a; j >= b; j--)
+                {
+
+                    if (i + j != c) counter++;
+                    
+                    if (i + j == c)
+                    {
+                        counter++;
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {c})");
+                        return;
+                    }
+
+                }
+
+            }
+            Console.WriteLine($"{counter} combinations - neither equals {c}");
+        }
+    }
+}
---
+++ TrainerSalary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace TrainerSalary
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lekcii = double.Parse(Console.ReadLine());
+            var budjet = double.Parse(Console.ReadLine());
+            var jelev = 0;
+            var royal = 0;
+            var roli = 0;
+            var trofon = 0;
+            var sino = 0;
+            var others = 0;
+            for (int i = 0; i < lekcii ; i++)
+            {
+                var lektor = Console.ReadLine();
+                if (lektor == "Jelev") jelev++;
+                if (lektor == "RoYaL") royal++;
+                if (lektor == "Roli") roli++;
+                if (lektor == "Trofon") trofon++;
+                if (lektor == "Sino") sino++;
+                if (lektor != "Jelev" && lektor != "RoYaL" && lektor != "Roli" && lektor != "Trofon" && lektor != "Sino") others++;
+            }
+            var jelevSalary = jelev * budjet / lekcii;
+            var royalSalary = royal  * budjet / lekcii;
+            var roliSalary = roli * budjet / lekcii;
+            var trofonSalary = trofon  * budjet / lekcii;
+            var sinoSalary = sino * budjet / lekcii;
+            var othersSalary = others  * budjet / lekcii;
+            Console.WriteLine($"Jelev salary: {jelevSalary:f2} lv");
+            Console.WriteLine($"RoYaL salary: {royalSalary:f2} lv");
+            Console.WriteLine($"Roli salary: {roliSalary:f2} lv");
+            Console.WriteLine($"Trofon salary: {trofonSalary:f2} lv");
+            Console.WriteLine($"Sino salary: {sinoSalary:f2} lv");
+            Console.WriteLine($"Others salary: {othersSalary:f2} lv");
+
+        }
+    }
+}
