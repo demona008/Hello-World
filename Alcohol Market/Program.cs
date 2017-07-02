a2d3802312103c5da4d663f48caf108dc6820e1a
Bozhidar <demona008@abv.bg>
2017-07-02 12:17:45 +0300

    Alcohol Market

---
+++ Alcohol Market/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Alcohol_Market
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cenaWhisky = double.Parse(Console.ReadLine());
+            var biraLitri = double.Parse(Console.ReadLine());
+            var vinoLitri = double.Parse(Console.ReadLine());
+            var rakiaLitri = double.Parse(Console.ReadLine());
+            var whiskyLitri = double.Parse(Console.ReadLine());
+            var cenaRakia = cenaWhisky / 2;
+            var cenaVino = 0.6 * cenaRakia;
+            var cenaBira = 0.2 * cenaRakia;
+            var totalPari = cenaWhisky * whiskyLitri + rakiaLitri * cenaRakia + vinoLitri * cenaVino + biraLitri * cenaBira;
+            Console.WriteLine($"{totalPari:f2}");
+
+        }
+    }
+}
