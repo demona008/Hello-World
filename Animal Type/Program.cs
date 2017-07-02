aabe2e3e2b45888500f41408054b940a915b30fc
Bozhidar <demona008@abv.bg>
2017-07-02 13:57:17 +0300

    Animal Type

---
+++ Animal Type/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Animal_Type
+{
+    class Program
+    {
+        static void Main()
+        {
+            string animal = Console.ReadLine().ToLower();
+
+            switch (animal)
+            {
+
+                case "dog":Console.WriteLine("mammal"); break;
+                case "crocodile":Console.WriteLine("reptile"); break;
+                case "tortoise":Console.WriteLine("reptile"); break;
+                case "snake":Console.WriteLine("reptile"); break;                  
+                default: if (animal!="dog" && animal!="crocodile" && animal!="tortoise" && animal!="snake")
+                     
+                     Console.WriteLine("unknown"); break;
+                                         
+            }
+        }
+    }
+}
