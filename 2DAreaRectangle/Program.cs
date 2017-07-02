dfb8a7360fd47fcc81a6995e23126ffe54e14b64
Bozhidar <demona008@abv.bg>
2017-07-02 12:10:42 +0300

    2DAreaRectangle

---
+++ 2DAreaRectangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace _2DAreaRectangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            var x1 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine());
+            var x2 = double.Parse(Console.ReadLine());
+            var y2 = double.Parse(Console.ReadLine());
+
+            var stranaX = Math.Abs(x1 - x2);
+            var stranaY = Math.Abs(y1 - y2);
+
+            var area = stranaX * stranaY;
+            var perimeter = 2 * stranaX + 2 * stranaY;
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+
+        }
+    }
+}
