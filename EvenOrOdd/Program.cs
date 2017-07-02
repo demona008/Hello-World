bd5aed916f071bd3411004b20a51a41f4c1f0a15
Bozhidar <demona008@abv.bg>
2017-07-02 14:10:13 +0300

    Add files via upload

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+            var even = (num % 2 == 0);
+            if ( even)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
---
+++ Excellent Result/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Excellent_Result
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = double.Parse(Console.ReadLine());
+
+            if ( a>= 5.50 && a<=6.00)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else
+            {
+                Console.WriteLine("Not excellent.");
+            }
+        }
+    }
+}
